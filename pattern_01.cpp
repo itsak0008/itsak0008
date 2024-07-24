fa36911014ccbe64a3a873af4a35b731e114bee1
Alok kumar <itsak0008@gmail.com>
2024-07-24 16:25:04 +0530

    patterns

---
+++ pattern_01.cpp
@@ -0,0 +1,35 @@
+#include<iostream>
+using namespace std;
+
+
+
+int main(){
+  int row , col;
+  cout<<"enter row "<<endl;
+  cin>>row;
+  cout<<"enter col "<<endl;
+  cin>>col;
+
+
+  for (int i = 0; i < row; i++)
+  {
+    if(i==0 || i==row-1 ){
+    for (int j = 0; j < col; j++)
+    { 
+      cout<<"* ";
+    }
+    }else{
+      cout<<"* ";
+      for (int k = 0; k < col-2; k++)
+      {
+        cout<<"  ";
+      }
+      cout<<"* ";
+
+      
+    }
+    cout<<endl;
+    
+  }
+  return 0;
+}
