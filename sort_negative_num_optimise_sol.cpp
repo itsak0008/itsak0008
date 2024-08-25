ee1c321e22336417df07dffb26ee94e602442da3
Alok kumar <itsak0008@gmail.com>
2024-08-25 18:09:25 +0530

    sort_negative_num_optimise_sol.cpp

---
+++ sort_negative_num_optimise_sol.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+#include <vector>
+#include <limits.h>
+#include <algorithm>
+using namespace std;
+int main()
+{
+    vector<int> arr{1, 2, -3, 4, -5, 6};
+    sort(arr.begin(), arr.end());
+    for (int i = 0; i < arr.size(); i++)
+    {
+        cout << arr[i] << " ";
+    }
+    return 0;
+}
