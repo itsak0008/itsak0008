7af806af82047a4078eaf9876beee9d02a60559a
Alok kumar <itsak0008@gmail.com>
2024-08-26 23:00:00 +0530

    binary_search.cpp

--- binary_search.cpp
+++ binary_search.cpp
@@ -4,7 +4,7 @@ using namespace std;
 binary_Number(int arr[],int size,int target){
     int start = 0;
     int end = size -1;
-    int mid = (start + end) / 2;
+    int mid =start + (end - start) / 2;
     while (start <= end)
     {
         if (arr[mid] == target)
@@ -15,7 +15,7 @@ binary_Number(int arr[],int size,int target){
         }else{
             start = mid + 1;
         }
-        mid = (start + end ) / 2;
+        mid = start + (end - start) / 2;
         
     }
     return -1;
