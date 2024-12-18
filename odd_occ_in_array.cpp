d15ff81af4993b91be9e9ef776ce32af514a7793
Alok kumar <itsak0008@gmail.com>
2024-12-18 10:42:41 +0530

    odd_occ_in_array.cpp

---
+++ odd_occ_in_array.cpp
@@ -0,0 +1,50 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+
+int solve(vector<int> arr)
+{
+    int start = 0;
+    int end = arr.size() - 1;
+
+    while (start <= end)
+    {
+        int mid = start + (end - start) / 2;
+        if (start == end)
+        {
+            return start;
+        }
+        if (mid % 2 == 0)
+        {
+            if (arr[mid] == arr[mid + 1])
+            {
+                start = mid + 2;
+            }
+            else
+            {
+                end = mid;
+            }
+        }
+        else
+        {
+            if (arr[mid] == arr[mid - 1])
+            {
+                start = mid + 1;
+            }
+            else
+            {
+                end = mid - 1;
+            }
+        }
+    }
+    return -1;
+}
+
+int main()
+{
+    vector<int> arr{1, 1, 2, 2, 3, 3, 4, 4, 7, 600, 600, 4, 4};
+    int result = solve(arr);
+    cout << "index is " << result << endl;
+    cout << "Ans is " << arr[result] << endl;
+    return 0;
+}
